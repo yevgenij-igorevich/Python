ea13522412705e942e6379665cc0b600f525d92f
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-06-21 22:06:50 +0300

    v.0.0.1

---
+++ #5 Lists.py
@@ -0,0 +1,12 @@
+# Списки
+spisok = [-3, 0, 4, 5.5, 'text', [4.4, [5, 9, 10], 6, 'text_2', 5]]
+print(spisok)
+
+names = ['Евгений', 'Игорь', 'Егор', 'Виталик', 'Роман']
+print(names[0])
+
+for name in names:
+    print(names)
+
+for name in names:
+    print(name)
