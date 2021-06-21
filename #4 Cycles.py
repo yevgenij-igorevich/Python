2975f9666b854d847618a8decaca496b1845c0f3
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-06-21 19:57:43 +0300

    v0.0.1

--- #4 Cycles.py
+++ #4 Cycles.py
@@ -6,3 +6,21 @@ for a in name:
 
 for b in range(1, 11):
     print(name)
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+a = 1
+while a <= 10:
+    print(a)
+    a = a + 1
+    break
+
+b = 1
+while b <= 10:
+    if b != 5:
+        print(b)
+    b = b + 1
+    continue
