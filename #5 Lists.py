6581f8ccc8a6cb1a82845f221c2e28244da08ced
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-06-22 19:14:10 +0300

    v0.0.1

--- #5 Lists.py
+++ #5 Lists.py
@@ -3,7 +3,7 @@ spisok = [-3, 0, 4, 5.5, 'text', [4.4, [5, 9, 10], 6, 'text_2', 5]]
 print(spisok)
 
 names = ['Евгений', 'Игорь', 'Егор', 'Виталик', 'Роман']
-print(names[0], [4])
+print(names[0])
 
 for name in names:
     print(names)
