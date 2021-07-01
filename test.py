8cce2c87f2407e381d12e6ccf49d4dbe4144a9ee
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-07-01 22:07:07 +0300

    v1

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
-x = int(input(''))
+# Вариации отрезков
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
 
-if x >= 0 and x <= 36:
-    if x == 0:
-        print('зеленый')
-    elif x > 0 and x <= 10:
-        if x % 2 == 0:
-            print('черный')
-        else:
-            print('красный')
-    elif x > 10 and x <= 18:
-        if x % 2 == 0:
-            print('красный')
-        else:
-            print('черный')
-    elif x > 18 and x <= 28:
-        if x % 2 == 0:
-            print('черный')
-        else:
-            print('красный')
-    elif x > 28 and x <= 36:
-        if x % 2 == 0:
-            print('красный')
-        else:
-            print('черный')
-else:
-    print('ошибка ввода')
+if a1 < b1 and a2 < b2:
+    if b1 < a2 or a1 > b2:  # Пустое множество
+        print('пустое множество')
+    elif b1 == a2:  # 1,5,5,10
+        print(b1)
+    elif a1 == b2:  # 5,10,1,5
+        print(a1)
+    elif a1 <= a2 < b1 < b2:
+        print(a2, b1)
+    elif a2 <= a1 < b2 < b1:
+        print(a1, b2)
+    elif a1 < a2 < b2 <= b1:
+        print(a2, b2)
+    else:
+        if a2 < a1 < b1 <= b2:
+            print(a1, b1)
+        elif a1 == a2 and b1 == b2:
+            print(a1, b1)
