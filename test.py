e6f478d4b2d7c7d9af7ff2e004f330a5f0c77674
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-07-01 22:28:33 +0300

    v2

--- test.py
+++ test.py
@@ -1,24 +1,12 @@
-# Вариации отрезков
-a1 = int(input())
-b1 = int(input())
-a2 = int(input())
-b2 = int(input())
-
-if a1 < b1 and a2 < b2:
-    if b1 < a2 or a1 > b2:  # Пустое множество
-        print('пустое множество')
-    elif b1 == a2:  # 1,5,5,10
-        print(b1)
-    elif a1 == b2:  # 5,10,1,5
-        print(a1)
-    elif a1 <= a2 < b1 < b2:
-        print(a2, b1)
-    elif a2 <= a1 < b2 < b1:
-        print(a1, b2)
-    elif a1 < a2 < b2 <= b1:
-        print(a2, b2)
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+if b1 < a2 or b2 < a1:
+    print('пустое множество')
+else:
+    if a1 > a2:
+        a2 = a1
+    if b1 > b2:
+        b1 = b2
+    if a2 == b1:
+        print(a2)
     else:
-        if a2 < a1 < b1 <= b2:
-            print(a1, b1)
-        elif a1 == a2 and b1 == b2:
-            print(a1, b1)
+        print(a2, b1)
