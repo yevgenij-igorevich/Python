c94d3af9be034dbf620e8dc7fe36c668ca76af2e
Yevgenij Igorevich <yevgenij.igorevich.work@gmail.com>
2021-07-07 10:47:44 +0300

    1

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
-if b1 < a2 or b2 < a1:
-    print('пустое множество')
-else:
-    if a1 > a2:
-        a2 = a1
-    if b1 > b2:
-        b1 = b2
-    if a2 == b1:
-        print(a2)
-    else:
-        print(a2, b1)
+x = 0
+for i in range(5, 1):
+    x = x + 1
+    print(i, 'text', x)
